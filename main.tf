81f82740760a357a86b3a77f9ed400624edcb218
Dmitry Gorbov <4911304+dgorbov@users.noreply.github.com>
2020-01-17 08:55:48 +0300

    DynamoDB costs optimisation

--- main.tf
+++ main.tf
@@ -25,8 +25,9 @@ resource "aws_s3_bucket" "tf_state" {
 resource "aws_dynamodb_table" "tf_state_lock" {
   name = "tf-state-lock-${var.s3_bucket_name}"
   hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
+  billing_mode = "PAY_PER_REQUEST"
+  # read_capacity = 20
+  # write_capacity = 20
   attribute {
     name = "LockID"
     type = "S"
